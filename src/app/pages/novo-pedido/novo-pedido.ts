004002d7ec2f75eafbf158803cd1d96076d11c77
Amanda Oliveira <amandaol7b@gmail.com>
2026-03-27 23:08:14 -0300

    initial commit

---
+++ src/app/pages/novo-pedido/novo-pedido.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-pedido',
+  imports: [],
+  templateUrl: './novo-pedido.html',
+  styleUrl: './novo-pedido.scss',
+})
+export class NovoPedido {}
